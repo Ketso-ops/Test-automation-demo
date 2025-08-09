dcfb8a58c87270af14784588c1251fe7a45c35d5
Ketso-ops <158741680+Ketso-ops@users.noreply.github.com>
2025-08-09 19:00:14 +0200

    Create test_login.py
    Add first Selenium test

---
+++ tests/test_login.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    yield driver
+    driver.quit()
+
+def test_valid_login(driver):
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.find_element(By.ID, "username").send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+    flash = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "flash")))
+    assert "You logged into a secure area!" in flash.text
